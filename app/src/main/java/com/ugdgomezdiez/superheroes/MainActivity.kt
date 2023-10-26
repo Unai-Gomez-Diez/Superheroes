314f68d84c967458651b4230bd39dfb6922c8a99
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-10-26 22:46:25 +0200

    Arreglando lo que me ha dicho Chema para subir la calidad del codigo

--- app/src/main/java/com/ugdgomezdiez/superheroes/MainActivity.kt
+++ app/src/main/java/com/ugdgomezdiez/superheroes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ugdgomezdiez.superheroes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ugdgomezdiez.superheroes.app.api.ApiClient
 import com.ugdgomezdiez.superheroes.data.remote.ApiRemoteDataSource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: SuperHeroeViewModel by lazy {
         SuperHeroeViewModel(
             ApiRemoteDataSource(
+                ApiClient()
             )
         )
     }
